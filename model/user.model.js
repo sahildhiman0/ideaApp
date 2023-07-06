db6ac43b269735ae4a87f9d265a12b16c2bfa167
sahildhiman0 <sahildhimaann@gmail.com>
2023-07-06 09:50:30 +0530

    connected to mongo db

--- model/user.model.js
+++ model/user.model.js
@@ -1,35 +1,48 @@
-// this will hold the schema for the user 
-// it explains the different fields of use and how it will be stored in the mongo database.
+/**
+ * This will hold the schema for the user
+ * 
+ * It explains the different fields of use and how it will be
+ * stored in the mongodb
+ */
 
 const mongoose = require('mongoose');
 
+
 const userSchema = new mongoose.Schema({
-    name :{
-        type : String,
-        required : true 
-    },
-    userId:{
-        type : String ,
-        required : true,
-        unique : true 
-    },
-    password : {
-        type: String,
-        required : true
-    },
-    emailId :{
-        type : String,
-        unique:true,
-        required: true,
-        minLength:10,
-        lowercase:true
 
-    },
-    userType : {
-         type : String,
-        unique:true,
-        default : "CUSTOMER",
-        enum:["customer","admin"]
+   name : {
+    type : String,
+    required : true
+   },
+   userId : {
+    type : String,
+    required : true,
+    unique : true
+   },
+   password : {
+    type : String,
+    required : true
+   },
+   email : {
+     type : String,
+     required : true,
+     unique : true,
+     minLength : 10,
+     lowercase : true
 
-    }
-},{timestamps: true});
+   },
+   userType : {
+    type : String,
+    required : true,
+    deafult : "CUSTOMER",
+    enum : ["CUSTOMER", "ADMIN"]
+   }
+   
+
+},{timestamps : true});
+
+/**
+ * Define the collection name where it will be stored
+ */
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 
-// const express = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
+const config = require('./configs/server.config');
 
 const app = express();
+// logic to connect to mongoDB and create an Admin user
 
-const config = require('./configs/server.config')
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection;
+
+db.on("error", ()=>{
+    console.log("Error while connecting to DB,solve asap");
+});
+
+db.once("open", ()=>{
+    console.log("DB is connected");
+});
+
 
 app.listen(config.PORT,()=>{
     console.log("server has started");
-})
+}); 
